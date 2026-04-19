d557e981b4f344cbc47a0056f1895705374a33ad
douglas <douglas@10.146.192.84>
2026-04-19 14:12:57 -0300

    init: Início do projeto.

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        String nome = "João";
+        System.out.println(nome);
+        nome = "Maria";
+        System.out.println("O valor da variável é: " + nome);
+    }
+}
